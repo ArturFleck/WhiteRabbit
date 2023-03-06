02ba604f5d894c3960dca5ab935f5709b2f1f868
Artur Fleck <artur.ivanishyn@gmail>
2023-03-06 16:40:10 +0200

    new Kata Square_Matrix_Multiplication

--- src/kata_s/Square_Matrix_Multiplication.java
+++ src/kata_s/Square_Matrix_Multiplication.java
@@ -1,4 +1,5 @@
 package kata_s;
+
 /*Write a function that accepts two square (NxN) matrices (two dimensional arrays), and returns the product of the two. Only square matrices will be given.
         How to multiply two square matrices:
         We are given two matrices, A and B, of size 2x2 (note: tests are not limited to 2x2). Matrix C, the solution, will be equal to the product of A and B. To fill in cell [0][0] of matrix C, you need to compute: A[0][0] * B[0][0] + A[0][1] * B[1][0].
@@ -21,34 +22,58 @@ package kata_s;
 public class Square_Matrix_Multiplication {
     public static void main(String[] args) {
         //int[][] a = {{1,2},{3,2}};
-        int[][] a = {   {1,2,3,2,1},
-                        {4,5,6,5,4},
-                        {7,8,9,8,7},
-                        {1,2,3,4,5}};
-        int[][] b = {{3,2},{1,1}};
+        int[][] a = {   {1, 2, 3, 2, 1},
+                        {4, 5, 6, 5, 4},
+                        {7, 8, 9, 8, 7},
+                        {1, 2, 3, 4, 5} };
 
-        int[][] c = new int [a[0].length][b.length];
-        c=matrixMultiplication(a,b);
+        int[][] b = {   {3, 2},
+                        {2, 3},
+                        {1, 2},
+                        {2, 1},
+                        {4, 5}  };
 
+        printMatrix(b);
 
-        matrixMultiplication(a,b);
+        System.out.println("--------------");
+        int[][] c = rotateMatrix(b);
+        printMatrix(c);
 
-        //System.out.println(a.length);
-        //System.out.println(a[0].length);
+        int [][] res = matrixMultiplication(a,b);
+        printMatrix(res);
 
-        for (int i=0; i< c.length;i++){
-            for (int j=0; j < c[0].length; j++){
-                System.out.print (c[i][j] + " ");
+    }
+
+    public static int[][] rotateMatrix(int[][] c) {
+        int[][] temp = new int[c[0].length][c.length];
+        for (int i = 0; i < c.length; i++) {
+            int k = 0;
+            for (int j = c[0].length-1; j >= 0; j--) {
+                temp[k][i] = c[i][j];
+                k++;
+            }
+        }
+        return temp;
+    }
+    public static void printMatrix(int [][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                System.out.print(m[i][j] + " ");
             }
             System.out.println();
         }
     }
-    public static int[][] matrixMultiplication(int[][] a, int[][] b) {
-        int[][] c = new int [a[0].length][b.length];
 
-        for (int i=0; i<a[0].length;i++){
-            for (int j=0;j<b.length;j++){
-                
+
+    public static int[][] matrixMultiplication(int[][] a, int[][] b) {
+        int[][] c = rotateMatrix(b);
+
+
+        int[][] d = new int[a[0].length][b.length];
+
+        for (int i = 0; i < a[0].length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                int sum = a[i][j] * b[j][i];
             }
         }
 
